23035cb8c11a8221f0af0b6c704238747b4e45d1
konabe <rt.konabe@gmail.com>
2024-03-01 22:42:23 +0900

    add SingleCheckBox story

---
+++ src/components/atoms/SingleCheckBox.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import SingleCheckBox from "./SingleCheckBox";
+import { useState } from "react";
+
+const meta = {
+  title: "components/atoms/SingleCheckBox",
+  component: SingleCheckBox,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof SingleCheckBox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    value: false,
+  },
+  render: ({ ...args }) => {
+    const [value, setValue] = useState<boolean>(args.value);
+    return (
+      <SingleCheckBox
+        value={value}
+        onChange={(v) => setValue(v)}
+        label="チェック"
+      />
+    );
+  },
+};
+
+export const On: Story = {
+  args: {
+    value: true,
+  },
+  render: ({ ...args }) => {
+    const [value, setValue] = useState<boolean>(args.value);
+    return (
+      <SingleCheckBox
+        value={value}
+        onChange={(v) => setValue(v)}
+        label="チェック"
+      />
+    );
+  },
+};
