b15a812b747bfb19a1631f1d7eb446a63dab980c
konabe <rt.konabe@gmail.com>
2024-03-04 22:56:28 +0900

    add ChemicalTag story& tests

--- src/domain/models/onsen/chemicalTagModel.ts
+++ src/domain/models/onsen/chemicalTagModel.ts
@@ -26,6 +26,25 @@ export class ChemicalTagModel extends ValueObject implements OmittableText {
     return new Chemical(this._value).getOmittedText();
   }
 
+  getImageColor(): string {
+    const colorDictionary: Record<ChemicalTagOption, string> = {
+      NaIon: "#007bff",
+      CaIon: "#28a745",
+      MgIon: "#17a2b8",
+      ClIon: "#ffc107",
+      HCO3Ion: "#dc3545",
+      SO4Ion: "#6610f2",
+      CO2: "#6f42c1",
+      FeIon: "#e83e8c",
+      HIon: "#20c997",
+      IIon: "#17a2b8",
+      S: "#343a40",
+      Rn: "#6c7500",
+      Simple: "#6c757d",
+    };
+    return colorDictionary[this._value];
+  }
+
   equals(vo: ChemicalTagModel): boolean {
     return this._value === vo.value;
   }
