af7c75a90e9d442423463ceb83810600b1a806c8
konabe <rt.konabe@gmail.com>
2024-03-08 09:33:08 +0900

    Added Codecov bundler plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 import path from "path";
 
 export default defineConfig({
   appType: "spa",
-  plugins: [react()],
+  plugins: [
+    react(), // Put the Codecov vite plugin after all other plugins
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.VITE_CODECOV_TOKEN !== undefined,
+      bundleName: "main",
+      uploadToken: process.env.VITE_CODECOV_TOKEN,
+    }),
+  ],
   resolve: {
     alias: { "@/": path.join(__dirname, "src") },
   },
