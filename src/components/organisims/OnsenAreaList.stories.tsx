3d03b80b6181c86ccd4d9380b980158d8e04b78a
konabe <rt.konabe@gmail.com>
2024-03-11 00:37:02 +0900

    add OnsenAreaList test & story

---
+++ src/components/organisims/OnsenAreaList.stories.tsx
@@ -0,0 +1,101 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import {
+  reactRouterParameters,
+  withRouter,
+} from "storybook-addon-react-router-v6";
+
+import { AreaEntity, AreaEntityParameter } from "../../domain/models/area";
+import { prefectures } from "../../share/prefecture";
+import OnsenAreaList from "./OnsenAreaList";
+
+const meta = {
+  title: "components/organisms/OnsenAreaList",
+  component: OnsenAreaList,
+  parameters: {
+    layout: "padded",
+    reactRouter: reactRouterParameters({}),
+  },
+  decorators: [withRouter],
+  tags: ["autodocs"],
+  argTypes: {
+    areas: { cotrol: "object" },
+    prefectures: { cotrol: "object" },
+  },
+} satisfies Meta<typeof OnsenAreaList>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const commonArea: AreaEntityParameter = {
+  id: 0,
+  name: "鳴子",
+  prefecture: "宮城県",
+  nationalResort: true,
+  village: "鳴子温泉",
+  url: "https://www.welcome-naruko.jp/",
+  description: "鳴子温泉は、宮城県大崎市鳴子温泉にある温泉。",
+  onsenIds: [],
+};
+
+export const Primary: Story = {
+  args: {
+    areas: [
+      new AreaEntity({
+        ...commonArea,
+        name: "鳴子",
+        prefecture: "宮城県",
+      }),
+      new AreaEntity({
+        ...commonArea,
+        name: "東鳴子",
+        prefecture: "宮城県",
+      }),
+      new AreaEntity({
+        ...commonArea,
+        name: "西鳴子",
+        prefecture: "宮城県",
+      }),
+      new AreaEntity({
+        ...commonArea,
+        name: "南鳴子",
+        prefecture: "宮城県",
+      }),
+      new AreaEntity({
+        ...commonArea,
+        name: "北鳴子",
+        prefecture: "宮城県",
+      }),
+      new AreaEntity({
+        ...commonArea,
+        name: "中鳴子",
+        prefecture: "宮城県",
+      }),
+      new AreaEntity({
+        ...commonArea,
+        name: "外鳴子",
+        prefecture: "宮城県",
+      }),
+      new AreaEntity({
+        ...commonArea,
+        name: "内鳴子",
+        prefecture: "宮城県",
+      }),
+      new AreaEntity({
+        ...commonArea,
+        name: "月岡",
+        prefecture: "新潟県",
+      }),
+      new AreaEntity({
+        ...commonArea,
+        name: "草津",
+        prefecture: "群馬県",
+      }),
+      new AreaEntity({
+        ...commonArea,
+        name: "伊香保",
+        prefecture: "群馬県",
+      }),
+    ],
+    prefectures: prefectures(),
+  },
+};
---
+++ src/test/components/organisms/OnsenAreaList.spec.tsx
@@ -0,0 +1,113 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+
+import OnsenAreaList from "../../../components/organisims/OnsenAreaList";
+import { AreaEntity, AreaEntityParameter } from "../../../domain/models/area";
+import { prefectures } from "../../../share/prefecture";
+
+describe("OnsenAreaList", () => {
+  it("should be displayed", () => {
+    const commonArea: AreaEntityParameter = {
+      id: 0,
+      name: "鳴子",
+      prefecture: "宮城県",
+      nationalResort: true,
+      village: "鳴子温泉",
+      url: "https://www.welcome-naruko.jp/",
+      description: "鳴子温泉は、宮城県大崎市鳴子温泉にある温泉。",
+      onsenIds: [],
+    };
+    render(
+      <OnsenAreaList
+        areas={[
+          new AreaEntity({
+            ...commonArea,
+            id: 1,
+            name: "鳴子",
+            prefecture: "宮城県",
+          }),
+          new AreaEntity({
+            ...commonArea,
+            id: 2,
+            name: "東鳴子",
+            prefecture: "宮城県",
+          }),
+          new AreaEntity({
+            ...commonArea,
+            id: 3,
+            name: "西鳴子",
+            prefecture: "宮城県",
+          }),
+          new AreaEntity({
+            ...commonArea,
+            id: 4,
+            name: "南鳴子",
+            prefecture: "宮城県",
+          }),
+          new AreaEntity({
+            ...commonArea,
+            id: 5,
+            name: "北鳴子",
+            prefecture: "宮城県",
+          }),
+          new AreaEntity({
+            ...commonArea,
+            id: 6,
+            name: "中鳴子",
+            prefecture: "宮城県",
+          }),
+          new AreaEntity({
+            ...commonArea,
+            id: 7,
+            name: "外鳴子",
+            prefecture: "宮城県",
+          }),
+          new AreaEntity({
+            ...commonArea,
+            id: 8,
+            name: "内鳴子",
+            prefecture: "宮城県",
+          }),
+          new AreaEntity({
+            ...commonArea,
+            id: 9,
+            name: "月岡",
+            prefecture: "新潟県",
+          }),
+          new AreaEntity({
+            ...commonArea,
+            id: 10,
+            name: "草津",
+            prefecture: "群馬県",
+          }),
+          new AreaEntity({
+            ...commonArea,
+            id: 11,
+            name: "伊香保",
+            prefecture: "群馬県",
+          }),
+          new AreaEntity({
+            ...commonArea,
+            id: 12,
+            name: "存在しないエリア",
+            prefecture: "存在しない県",
+          }),
+        ]}
+        prefectures={prefectures()}
+      />,
+      { wrapper: MemoryRouter }
+    );
+    expect(screen.getByText("宮城県")).toBeInTheDocument();
+    expect(screen.getByText("鳴子温泉")).toBeInTheDocument();
+    expect(screen.getByText("鳴子温泉")).toHaveAttribute("href", "/area/1");
+    expect(screen.getByText("東鳴子温泉")).toBeInTheDocument();
+    expect(screen.getByText("東鳴子温泉")).toHaveAttribute("href", "/area/2");
+    expect(screen.getByText("群馬県")).toBeInTheDocument();
+    expect(screen.getByText("草津温泉")).toBeInTheDocument();
+    expect(screen.getByText("草津温泉")).toHaveAttribute("href", "/area/10");
+    expect(screen.getByText("新潟県")).toBeInTheDocument();
+    expect(screen.getByText("月岡温泉")).toBeInTheDocument();
+    expect(screen.queryByText("存在しない県")).not.toBeInTheDocument();
+    expect(screen.queryByText("存在しないエリア")).not.toBeInTheDocument();
+  });
+});
