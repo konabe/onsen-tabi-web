2b0133a21d469d51c05a7be465d7a3a9eeab02b0
konabe <rt.konabe@gmail.com>
2024-03-10 23:54:45 +0900

    add OnsenCard test

--- src/components/organisims/OnsenCard.stories.tsx
+++ src/components/organisims/OnsenCard.stories.tsx
@@ -7,34 +7,48 @@ const meta = {
   title: "components/organisms/OnsenCard",
   component: OnsenCard,
   parameters: {
-    layout: "centered",
+    layout: "padded",
   },
   tags: ["autodocs"],
   argTypes: {
-    onsen: { control: "text" },
+    onsen: { control: "object" },
   },
 } satisfies Meta<typeof OnsenCard>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const commonEntity = () =>
+  new OnsenEntity({
+    id: 1,
+    name: "大滝乃湯",
+    generatedSpringQuality: "ナトリウム塩化物泉",
+    userSpringQuality: "",
+    chemicals: ["NaIon", "ClIon"],
+    liquid: "mildly_alkaline",
+    osmoticPressure: "isotonic",
+    temperature: "hot",
+    form: "sotoyu",
+    isDayUse: true,
+    url: "https://onsen-kusatsu.com/ohtakinoyu/",
+    imgURL: "https://placehold.jp/150x150.png",
+    description:
+      "徐々に体を慣らしながら熱いお湯に浸かるための合わせ湯を楽しむことができる。\nその独特の空気感はまさにテーマパークのよう。\n大浴場も広々としていて、まさに草津的な余裕感に癒される。\n白濁の日には清掃によって剥がされた湯の花が一斉に解き放たれる。贅沢な気分になりたいのであれば狙って通うとよい。",
+  });
+
 export const Primary: Story = {
   args: {
-    onsen: new OnsenEntity({
-      id: 1,
-      name: "大滝乃湯",
-      generatedSpringQuality: "ナトリウム塩化物泉",
-      userSpringQuality: "",
-      chemicals: ["NaIon", "ClIon"],
-      liquid: "mildly_alkaline",
-      osmoticPressure: "isotonic",
-      temperature: "hot",
-      form: "sotoyu",
-      isDayUse: true,
-      url: "https://onsen-kusatsu.com/ohtakinoyu/",
-      imgURL: "https://placehold.jp/150x150.png",
-      description:
-        "徐々に体を慣らしながら熱いお湯に浸かるための合わせ湯を楽しむことができる。\nその独特の空気感はまさにテーマパークのよう。\n大浴場も広々としていて、まさに草津的な余裕感に癒される。\n白濁の日には清掃によって剥がされた湯の花が一斉に解き放たれる。贅沢な気分になりたいのであれば狙って通うとよい。",
-    }),
+    onsen: commonEntity(),
   },
+};
+
+export const Small: Story = {
+  args: {
+    onsen: commonEntity(),
+  },
+  decorators: (Story) => (
+    <div style={{ width: "300px" }}>
+      <Story />
+    </div>
+  ),
 };
---
+++ src/test/components/organisms/OnsenCard.spec.tsx
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/react";
+
+import OnsenCard from "../../../components/organisims/OnsenCard";
+import { OnsenEntity } from "../../../domain/models/onsen";
+
+describe("OnsenCard", () => {
+  it("should be displayed", () => {
+    render(
+      <OnsenCard
+        onsen={
+          new OnsenEntity({
+            id: 1,
+            name: "大滝乃湯",
+            generatedSpringQuality: "ナトリウム塩化物泉",
+            userSpringQuality: "",
+            chemicals: ["NaIon", "ClIon"],
+            liquid: "mildly_alkaline",
+            osmoticPressure: "isotonic",
+            temperature: "hot",
+            form: "sotoyu",
+            isDayUse: true,
+            url: "https://onsen-kusatsu.com/ohtakinoyu/",
+            imgURL: "https://placehold.jp/150x150.png",
+            description:
+              "徐々に体を慣らしながら熱いお湯に浸かるための合わせ湯を楽しむことができる。",
+          })
+        }
+      />
+    );
+    const link = screen.getByText("🔗");
+    expect(screen.getByText("大滝乃湯")).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        "徐々に体を慣らしながら熱いお湯に浸かるための合わせ湯を楽しむことができる。"
+      )
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("(等張性・弱アルカリ性・高温泉)")
+    ).toBeInTheDocument();
+    expect(screen.getByText("ナ")).toBeInTheDocument();
+    expect(screen.getByText("塩")).toBeInTheDocument();
+    expect(link.tagName).toBe("A");
+    expect(link).toHaveAttribute(
+      "href",
+      "https://onsen-kusatsu.com/ohtakinoyu/"
+    );
+  });
+});
