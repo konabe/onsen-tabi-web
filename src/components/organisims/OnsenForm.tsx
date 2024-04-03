91f0d7026885c59526ec6ceacc1b72ef3ad28255
konabe <rt.konabe@gmail.com>
2024-04-04 06:22:57 +0900

    fix sonar issue

--- src/components/organisims/OnsenForm.tsx
+++ src/components/organisims/OnsenForm.tsx
@@ -223,7 +223,7 @@ const OnsenForm: React.FC<Props> = ({
 
   return (
     <SCreateCormContainer>
-      <form role="form">
+      <form>
         <SFieldSet>
           {formTitle !== undefined ? <legend>{formTitle}</legend> : undefined}
           <div>
